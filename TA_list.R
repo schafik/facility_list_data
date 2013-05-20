25b3b8e635d894a5b1f3b5e378c5b15919137beb
Zaiming Yao <zm.yao88@gmail.com>
2013-05-20 15:48:42 -0400

    TA list to record if a TA's matching result has been checked

---
+++ TA_list.R
@@ -0,0 +1,38 @@
+require(stringr)
+
+nc <- read.csv('./in_process_data/facility_lists/Data Matcher/back check/education_nc.csv')
+ne <- read.csv('./in_process_data/facility_lists/Data Matcher/back check/education_ne.csv')
+nw <- read.csv('./in_process_data/facility_lists/Data Matcher/back check/education_nw.csv')
+se <- read.csv('./in_process_data/facility_lists/Data Matcher/back check/education_se.csv')
+ss <- read.csv('./in_process_data/facility_lists/Data Matcher/back check/education_ss.csv')
+sw <- read.csv('./in_process_data/facility_lists/Data Matcher/back check/education_sw.csv')
+
+checked_list <- rbind(nc,ne,nw,se,ss,sw)
+# names(checked_list)
+checked_list <- subset(checked_list, select=c('lga_id.x', 'ta_name'))
+checked_list <- rename(checked_list, c('lga_id.x' = 'lga_id'))
+checked_list <- checked_list[!duplicated(checked_list$ta_name),]
+checked_list <- checked_list$ta_name
+
+checked_list <- gsub('[.]', '',checked_list)
+#checked_list <- gsub(' +', ' ',checked_list)
+checked_list <- tolower(checked_list)
+checked_list <- trim(checked_list)
+
+
+ta_list <- read.csv('./in_process_data/facility_lists/ta_list_checked.csv', stringsAsFactors=F)
+ta_list$ta_name <- gsub('[.]', '',ta_list$ta_name)
+#ta_list$ta_name <- gsub(' +', ' ',ta_list$ta_name)
+ta_list$ta_name <- tolower(ta_list$ta_name)
+ta_list$ta_name <- trim(ta_list$ta_name)
+
+#add column that checks if the TA's is being checked
+ta_list$checked <- ta_list$ta_name %in% checked_list
+
+checked_list[which(!(checked_list %in% ta_list$ta_name))]
+checked_list[order(as.character(checked_list))]
+
+
+
+write.csv(ta_list, './in_process_data/facility_lists/ta_list_checked.csv', row.names=F)
+
